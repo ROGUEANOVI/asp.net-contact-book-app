c1dfd08e6ac23a97e6eccd50a57cc35e4b60d4fa
ROGUEANOVI <ovidioromero66@gmail.com>
2023-11-02 19:07:06 -0500

    feat: Create project

---
+++ ContactBookApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactBookApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
