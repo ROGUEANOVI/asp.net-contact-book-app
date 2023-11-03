6cdc80bc96bdfbb922d5ac7f003f508158dab2aa
ROGUEANOVI <ovidioromero66@gmail.com>
2023-11-02 20:52:11 -0500

    feat: Add Contact model

---
+++ ContactBookApp/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactBookApp.Models
+{
+    [Table(name: "Contact")]
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "EL nombre es requerido")]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "EL telefono es requerido")]
+        [Display(Name = "Telefono")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "EL celular es requerido")]
+        [Display(Name = "Celular")]
+        public string CellPhone { get; set; }
+
+        [EmailAddress(ErrorMessage = "La dirección de correo electrónico no es válida")]
+        [Display(Name = "Correo Electronico")]
+        public string Email { get; set; }
+
+        [Display(Name = "Fecha de creacion")]
+        public DateTime CreationDate { get; set; }
+    }
+}
